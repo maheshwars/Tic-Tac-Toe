b1738bad14517ff02384ba6f6d5d0205a28f70e8
Maheshwar Singh <39465243+maheshwars@users.noreply.github.com>
2020-09-10 02:52:26 +0530

    Add files via upload

---
+++ player/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+from .forms import InvitationForm
+from .models import Invitation
+from gameplay.models import Game
+
+
+@login_required()
+def home(request):
+    my_games = Game.objects.games_for_user(request.user)
+    active_games = my_games.active()
+    finished_games = my_games.difference(active_games)
+    invitations = request.user.invitations_received.all()
+    return render(request, "player/home.html",
+                  {'active_games': active_games,
+                   'finished_games': finished_games,
+                   'invitations': invitations})
+
+
+@login_required()
+def new_invitation(request):
+    if request.method == "POST":
+        invitation = Invitation(from_user=request.user)
+        form = InvitationForm(instance=invitation, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('player_home')
+    else:
+        form = InvitationForm()
+    return render(request, "player/new_invitation_form.html", {'form': form})
+
+
+@login_required()
+def accept_invitation(request, id):
+    invitation = get_object_or_404(Invitation, pk=id)
+    if not request.user == invitation.to_user:
+        raise PermissionDenied
+    if request.method == 'POST':
+        if "accept" in request.POST:
+            game = Game.objects.create(
+                first_player=invitation.to_user,
+                second_player=invitation.from_user,
+            )
+        invitation.delete()
+        return redirect(game)# django will automatically use get_absolute_url function in class game of gameplay/models.py
+    else:
+        return render(request,
+                      "player/accept_invitation_form.html",
+                      {'invitation': invitation}
+                      )
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "player/signup_form.html"
+    success_url = reverse_lazy('player_home')
+
+
+
+    
+    # writing too much model code in view is not a good practise.
+    # So we dont use the below code instead write similar in models.py
+    """games_first_player = Game.objects.filter(
+        first_player = request.user,
+        status= 'F'
+    )
+    games_second_player = Game.objects.filter(
+        second_player = request.user, 
+        status = 'S'
+    )
+
+    all_my_games = list(games_first_player)+list(games_second_player)
+
+
+    return render(request, "player/home.html",
+                {'games': all_my_games})"""
+
+
